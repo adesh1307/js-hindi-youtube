1cd0cdb65a63787dcb8f5e117da2efcd3d3b2794
adesh1307 <adeshdhavale1307@gmail.com>
2024-06-05 12:33:49 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use Strict"; // treat all code as newer version 
+
+// alert (3+3) //  alert is used in browser ,we are using nodejs, not browser
+
+console.log(3+3)
+console.log("adesh")
+
+let name = "adesh"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// types of data type 
+// number=> 2 to power 53
+// bigint => for handling big no of data
+// " " => string data type
+// boolean => only tells true or false
+//  null => standalone value
+// undefined => value which is not defined
+
+
+// object
+
+//  console.log(typeof);      // syntax for identifying type of data type 
+
+console.log(typeof"adesh");   // type of "" is string 
+
+console.log (typeof 21)       // type of 21 is number
+
+console.log (typeof null)      //type of null is object 
+
+console.log (typeof undefined)  // type of undefined is undefined 
+
+
+

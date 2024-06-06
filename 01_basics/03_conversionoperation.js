6bdb18c57a17ba59f794c2bb5e43cd841baf92a0
adesh1307 <adeshdhavale1307@gmail.com>
2024-06-06 04:38:31 +0000

    datatypeconversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log ( typeof score);
+
+ let valueInNumber = Number (score) 
+
+ // let valueInNumber = Number ( )  is used for conversion from string to number
+
+
+ console.log(typeof valueInNumber);

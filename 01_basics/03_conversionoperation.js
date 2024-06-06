7728511b6e4dcb331c1414d542faf4a3db860d97
adesh1307 <adeshdhavale1307@gmail.com>
2024-06-06 15:08:09 +0000

    conversionoperation.js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -8,3 +8,29 @@ console.log ( typeof score);
 
 
  console.log(typeof valueInNumber);
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1 ; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+// 1 => True; 0 => False
+// " " = False  if nothing is written o/p is false
+// "Adesh" => true
+
+
+let someNumber = 33
+
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
